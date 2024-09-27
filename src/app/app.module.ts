4392b91d080038103d135eac2f3a3ab82175ab69
Kehui Zhao <kehui.zhao@gmail.com>
2024-09-26 23:01:16 -0400

    added LocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
